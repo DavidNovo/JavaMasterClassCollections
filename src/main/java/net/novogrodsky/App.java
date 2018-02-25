bc579f6bce554749daa1d7f531d382b5d5743609
David Novogrodsky <david.novogrodsky@gmail.com>
2018-02-24 18:47:13 -0600

    creating solution for java.util.Array problem

--- src/main/java/net/novogrodsky/App.java
+++ src/main/java/net/novogrodsky/App.java
@@ -1,13 +1,75 @@
 package net.novogrodsky;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
+
 /**
- * Hello world!
- *
+ * Hello world! .
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+  // this scans the command line and accepts user input
+  private static Scanner scanner = new Scanner(System.in);
+
+  /**
+   * The main method 'nuff said.
+   */
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+
+    // first part, using the builtin Arrays
+    int[] myIntegers = getIntegers(5);
+    int[] sorted = sortIntegers(myIntegers);
+    printArray(sorted);
+  }
+
+  /**
+   * This gets the input from the command line.
+   */
+  public static int[] getIntegers(int capacity) {
+    int[] array = new int[capacity];
+    System.out.println("Enter " + capacity + " integer values:\r");
+    for (int i = 0; i < array.length; i++) {
+      array[i] = scanner.nextInt();
     }
+    return array;
+  }
+
+  /**
+   * This outputs the results.
+   * @param array The values printed to the console.
+   */
+  public static void printArray(int[] array) {
+    for (int i = 0; i < array.length; i++) {
+      System.out.println("Element " + i + " contents " + array[i]);
+    }
+  }
+
+  /**
+   * This sorts the integers the user entered on the command line.
+   * @param array The integers entered on the command line.
+   * @return
+   */
+  public static int[] sortIntegers(int[] array) {
+    int[] sortedArray = Arrays.copyOf(array, array.length);
+
+    boolean flag = true;
+    int temp;
+    while (flag) {
+      flag = false;
+
+      for (int i = 0; i < sortedArray.length - 1; i++) {
+        if (sortedArray[i] < sortedArray[i + 1]) {
+          temp = sortedArray[i];
+          sortedArray[i] = sortedArray[i + 1];
+          sortedArray[i + 1] = temp;
+          flag = true;
+        }
+      }
+    }
+
+    return sortedArray;
+  }
+
 }
--- src/test/java/net/novogrodsky/AppTest.java
+++ src/test/java/net/novogrodsky/AppTest.java
@@ -3,36 +3,74 @@ package net.novogrodsky;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.Ignore;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest
+    extends TestCase {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+  /**
+   * Create the test case.
+   *
+   * @param testName name of the test case
+   */
+  public AppTest(String testName) {
+    super(testName);
+  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+  /**
+   * Default test suite.
+   * @return the suite of tests being tested
+   */
+  public static Test suite() {
+    return new TestSuite(AppTest.class);
+  }
+
+  /**
+   * Rigourous Test :-) .
+   */
+  public void testApp() {
+    assertTrue(true);
+  }
+
+  @Ignore
+  // This test is not needed
+  public void testMain() {
+  }
+
+  @Ignore
+  // This is the command line getting numbers, better for integration tests
+  // tests that can mimic human interaction.
+  public void testGetIntegers() {
+  }
+
+
+  /**
+   * This makes sure the print array function created the desired output.
+   */
+  public void testPrintArray() {
+    // create test array
+    int[] testIntArray = new int[]{1, 4, 33, 67, 243, 11};
+
+    // call method under test
+    App.printArray(testIntArray);
+  }
+
+  /**
+   * This makes sure that the elements of an array are ordered correctly.
+   */
+  public void testSortIntegers() {
+    // create test array
+    int[] testIntArray = new int[]{1, 4, 33, 67, 243, 11};
+
+    // call method under test
+    int[] sortedArray = App.sortIntegers(testIntArray);
+
+    // test that the order is correct
+    assertTrue(
+        "The largest integer should be at position 0.  In this case, the value should be 243",
+        sortedArray[0] == 243);
+  }
 }
