ae11a2f77aae13781f8c3268c0758719aa989907
David Novogrodsky <david.novogrodsky@gmail.com>
2018-04-19 21:14:25 -0500

    fixed problem with program accepting multi-word commands.

--- src/main/java/net/novogrodsky/adventuregame/Main.java
+++ src/main/java/net/novogrodsky/adventuregame/Main.java
@@ -5,8 +5,13 @@ import net.novogrodsky.adventuregame.Location;
 
 
 public class Main {
+
   private static Map<Integer, Location> locations = new HashMap<Integer, Location>();
 
+  /**
+   * The main method of the game.
+   * @param args Default for command line arguments.
+   */
   public static void main(String[] args) {
     // Change the program to allow players to type full words, or phrases, then move to the
     // correct location based upon their input.
@@ -19,32 +24,36 @@ public class Main {
     Scanner scanner = new Scanner(System.in);
 
     Map<String, Integer> tempExit = new HashMap<String, Integer>();
-    locations.put(0, new Location(0, "You are sitting in front of a computer learning Java",tempExit));
+    locations
+        .put(0, new Location(0, "You are sitting in front of a computer learning Java", tempExit));
 
     tempExit = new HashMap<String, Integer>();
     tempExit.put("W", 2);
     tempExit.put("E", 3);
     tempExit.put("S", 4);
     tempExit.put("N", 5);
-    locations.put(1, new Location(1, "You are standing at the end of a road before a small brick building",tempExit));
+    locations.put(1,
+        new Location(1, "You are standing at the end of a road before a small brick building",
+            tempExit));
 
     tempExit = new HashMap<String, Integer>();
     tempExit.put("N", 5);
-    locations.put(2, new Location(2, "You are at the top of a hill",tempExit));
+    locations.put(2, new Location(2, "You are at the top of a hill", tempExit));
 
     tempExit = new HashMap<String, Integer>();
     tempExit.put("W", 1);
-    locations.put(3, new Location(3, "You are inside a building, a well house for a small spring",tempExit));
+    locations.put(3,
+        new Location(3, "You are inside a building, a well house for a small spring", tempExit));
 
     tempExit = new HashMap<String, Integer>();
     tempExit.put("N", 1);
     tempExit.put("W", 2);
-    locations.put(4, new Location(4, "You are in a valley beside a stream",tempExit));
+    locations.put(4, new Location(4, "You are in a valley beside a stream", tempExit));
 
     tempExit = new HashMap<String, Integer>();
     tempExit.put("S", 1);
     tempExit.put("W", 2);
-    locations.put(5, new Location(5, "You are in the forest",tempExit));
+    locations.put(5, new Location(5, "You are in the forest", tempExit));
 
     Map<String, String> vocabulary = new HashMap<String, String>();
     vocabulary.put("QUIT", "Q");
@@ -53,37 +62,35 @@ public class Main {
     vocabulary.put("WEST", "W");
     vocabulary.put("EAST", "E");
 
-
     int loc = 1;
-    while(true) {
+    while (true) {
       System.out.println(locations.get(loc).getDescription());
       tempExit.remove("S");
 
-      if(loc == 0) {
+      if (loc == 0) {
         break;
       }
 
       Map<String, Integer> exits = locations.get(loc).getExits();
       System.out.print("Available exits are ");
-      for(String exit: exits.keySet()) {
+      for (String exit : exits.keySet()) {
         System.out.print(exit + ", ");
       }
       System.out.println();
 
       String direction = scanner.nextLine().toUpperCase();
-      if(direction.length() > 1) {
+      if (direction.length()  > 1) {
         String[] words = direction.split(" ");
-        for(String word: words) {
-          if(vocabulary.containsKey(word)) {
+        for (String word : words) {
+          if (vocabulary.containsKey(word)) {
             direction = vocabulary.get(word);
             break;
           }
         }
       }
 
-      if(exits.containsKey(direction)) {
+      if (exits.containsKey(direction)) {
         loc = exits.get(direction);
-
       } else {
         System.out.println("You cannot go in that direction");
       }
