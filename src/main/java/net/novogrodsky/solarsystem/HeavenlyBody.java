b2fe245b6d7dea744629c14411fca217a13aa90e
David Novogrodsky <david.novogrodsky@gmail.com>
2018-04-25 22:13:24 -0500

    changing the equals method

--- src/main/java/net/novogrodsky/solarsystem/HeavenlyBody.java
+++ src/main/java/net/novogrodsky/solarsystem/HeavenlyBody.java
@@ -7,62 +7,53 @@ import java.util.Set;
  * Created by dev on 12/01/2016.
  */
 public final class HeavenlyBody {
-    private final String name;
-    private final double orbitalPeriod;
-    private final Set<HeavenlyBody> satellites;
 
-    public HeavenlyBody(String name, double orbitalPeriod) {
-        this.name = name;
-        this.orbitalPeriod = orbitalPeriod;
-        this.satellites = new HashSet<>();
+  private final String name;
+  private final double orbitalPeriod;
+  private final Set<HeavenlyBody> satellites;
+
+  public HeavenlyBody(String name, double orbitalPeriod) {
+    this.name = name;
+    this.orbitalPeriod = orbitalPeriod;
+    this.satellites = new HashSet<>();
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public double getOrbitalPeriod() {
+    return orbitalPeriod;
+  }
+
+  public boolean addMoon(HeavenlyBody moon) {
+    return this.satellites.add(moon);
+  }
+
+  public Set<HeavenlyBody> getSatellites() {
+    return new HashSet<>(this.satellites);
+  }
+
+
+  // to make sure I am overriding, use parm of Object.
+  @Override
+  public boolean equals(Object obj) {
+    // this is referencial equality
+    // if both references point to the same object, they are equal
+    if (this == obj) {
+      return true;
     }
 
-    public String getName() {
-        return name;
+    // checking for null objects and checking for the class type
+    System.out.println("obj.getClass() is " + obj.getClass());
+    System.out.println("this.getClass() is " + this.getClass());
+    if ((obj == null) || (obj.getClass() != this.getClass())) {
+      return false;
     }
 
-    public double getOrbitalPeriod() {
-        return orbitalPeriod;
-    }
-
-    public boolean addMoon(HeavenlyBody moon) {
-        return this.satellites.add(moon);
-    }
-
-    public Set<HeavenlyBody> getSatellites() {
-        return new HashSet<>(this.satellites);
-    }
-
-    public boolean equals(Object obj) {
-        if(this == obj) {
-            return true;
-        }
-
-        System.out.println("obj.getClass() is " + obj.getClass());
-        System.out.println("this.getClass() is " + this.getClass());
-        if ((obj == null) || (obj.getClass() != this.getClass())) {
-            return false;
-        }
-
-        String objName = ((HeavenlyBody) obj).getName();
-        return this.name.equals(objName);
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    String objName = ((HeavenlyBody) obj).getName();
+    return this.name.equals(objName);
+  }
 
 
 }
